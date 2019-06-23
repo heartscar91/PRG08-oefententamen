194e59ca7e2f867acac688717183acebe4360388
Erik <erik@erik-hr.lan>
2019-06-23 23:21:40 +0200

    code fix

--- dev/towerbutton.ts
+++ dev/towerbutton.ts
@@ -1,24 +1,28 @@
-class TowerButton extends Button {
+class TowerButton {
     
     private progress:number = 0;
     private bar:HTMLElement;
     private game:Game;
-    constructor(game) {
-        super("towerbutton");
+    private pause: boolean = false;
+    protected div: HTMLElement;
+
+    constructor(game:Game) {
+        this.div = <HTMLElement>document.getElementsByTagName("towerbutton")[0];
+        this.div.addEventListener("click", (e: MouseEvent) => this.handleClick(e));
         this.game = game;
 
         this.bar = <HTMLElement> document.querySelector("towerbutton progressbar");
         this.bar.style.width = "0%";
     }
 
-    protected handleClick(event: MouseEvent) : void {
+    private handleClick(event: MouseEvent) : void {
         
         this.progress+=10;
         this.bar.style.width = this.progress+"%";
 
         if(this.progress > 90){
             this.progress = 0;
-            super.handleClick(event);
+            console.log("progress!");
             this.upgrade();
         }
     }
